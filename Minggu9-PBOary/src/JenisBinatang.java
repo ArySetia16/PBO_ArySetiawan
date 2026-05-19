ac0f49077421a2fec337855dd9d47dd7809fc86e
ArySetiawan <Setiawanary1611@gmail.com>
2026-05-19 20:00:42 +0700

    Add files via upload

---
+++ Minggu9-PBOary/src/JenisBinatang.java
@@ -0,0 +1,14 @@
+class jenisBinatang extends Binatang
+{
+    String jenisBinatang;
+    jenisBinatang(String jenisBinatang)
+    {
+        super(jenisBinatang);
+    }
+    public void cetakjenis()
+    {
+        super.cetakjenis();
+        jenisBinatang="Predator";
+        System.out.println("Jenis Binatang : " + jenisBinatang);
+    }
+}
